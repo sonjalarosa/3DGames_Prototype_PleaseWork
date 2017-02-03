243efb4532cdaf02a0a1cc92ecff21f00d0e7ec1
sonjalarosa <slarosa@mica.edu>
2017-02-03 09:40:02 -0500

    Prototype_Update
    WORKKKKK

---
+++ 3DGames_Week1_Prototype/Assets/Scripts/jump.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    public float thrust;
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision!");
+            col.gameObject.GetComponent<Rigidbody>().AddForce(0, thrust, 0);
+        }
+            }
+}
---
+++ 3DGames_Week1_Prototype/Assets/Scripts/restartLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartLevel : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
